a84f4025206aa410edbd70f10b6a90b94809e811
Daniel Araujo <daniel.araujo@mmda.com.br>
2019-09-02 17:54:48 -0300

    Ajustes e refatoramento

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,7 @@
 //Carregando Datatables
 $(document).ready(function() {
 	function loadUserTable(){
-        $('#tbUsuarios').DataTable({
-            "pageLength" : 10,
-            "filter" : true,
-            "deferRender" : true,
-            "scrollY" : 200,
-            "scrollCollapse" : true,
-            "scroller" : true,
-            "language": {
-                "lengthMenu": "Mostrando _MENU_ registros por página",
-                "zeroRecords": "Nada encontrado",
-                "info": "Mostrando página _PAGE_ de _PAGES_",
-                "infoEmpty": "Nenhum registro disponível",
-                "infoFiltered": "(filtrado de _MAX_ registros no total)"
-            }
-        });
+        
     }
 });
 
