90e729b155e4575e8e020df315801c21c2087a11
Dan Araujo <40579917+dancodeweb@users.noreply.github.com>
2019-10-03 17:17:35 -0300

    Update template.php

--- views/template.php
+++ views/template.php
@@ -9,7 +9,7 @@
     <title>Prymarya 2 framework</title>
     <meta name="author" content="Daniel Araujo" />
     <meta name="description" content="Prymarya 2 framework.">
-    <meta name="keywords" content="Gestão, escolar, web, HTML, CSS, Javascript" />
+    <meta name="keywords" content="Framework em PHP e Javascript" />
     <meta name="revised" content="Dancode, 19/09/2019" />
     <link rel="shortcut icon" type="image/x-icon" href="<?php echo BASEURL; ?>assets/img/icon.png">
     <link href="<?php echo BASEURL; ?>assets/lib/dataTables/css/jquery.dataTables.min.css" rel="stylesheet" />
